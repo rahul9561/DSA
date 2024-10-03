0bdef8a3cb5474ab95c4f2627356a3d06708ed67
rahul9561 <109431579+rahul9561@users.noreply.github.com>
2024-10-03 15:57:22 +0530

    arrylist

---
+++ Dsa/CollectionFrameworkwithjava/ArrayList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    List<Integer> l=new ArrayList<>();
+	    l.add(3);
+	     l.add(6);
+	      l.add(5);
+	       l.add(4);
+	       
+	       System.out.print(l);
+	       l.remove(2);
+	       System.out.print(l);
+	}
+}
